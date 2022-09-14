ded6794910652610aba8c97ec30dcf2253b3749c
Daniel Morais De Oliveira <danielmoraisdeoliveira1993@gmail.com>
2022-09-14 19:19:31 -0300

    Requisiçao numero 18 completa

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const buttonSubmit = document.querySelector('#submit-button');
-<<<<<<< HEAD
 const buttonSend = document.getElementById('submit-btn');
 const checkBoxAgree = document.querySelector('#agreement');
+const textArea = document.querySelector('#textarea');
+const pCounter = document.querySelector('#counter');
 buttonSend.disabled = true;
 function turnCheck() {
   buttonSend.disabled = !this.checked;
 }
-checkBoxAgree.addEventListener('change', turnCheck);
-=======
-const textArea = document.querySelector('#textarea');
-const pCounter = document.querySelector('#counter');
 
 function contaCaracteres() {
   const contador = 500 - textArea.value.length;
   pCounter.innerHTML = contador.toString();
 }
 
->>>>>>> 956558517d9ef891e40e198667cf8ff6620cf3f6
 function validaInformacoes() {
   if (emailInput.value === 'tryber@teste.com' && passwordInput.value.toString() === '123456') {
     alert('Olá, Tryber!');
@@ -35,3 +31,4 @@ buttonSubmit.addEventListener('click', clicaBotao);
 
 textArea.addEventListener('keyup', contaCaracteres);
 textArea.addEventListener('keydown', contaCaracteres);
+checkBoxAgree.addEventListener('change', turnCheck);
